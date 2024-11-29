4393eb2842faf5e0825579119092e3424f074ebc
michael.lin <michael@serviceforit.com>
2024-11-29 17:09:23 +0800

    add two floating icon

--- script.js
+++ script.js
@@ -77,3 +77,95 @@ depositGuideIcon.addEventListener("mouseout", () => {
 const img = document.getElementsByName("title");
 img.src = "../images/lineage_title.png?v=" + new Date().getTime();
 //-----------------------把圖票加入時間戳-------------------------------------
+
+//-----------------------Floating icon---------------------------------------
+// 選取圖片元素
+const contactUs = document.getElementById("contact_us");
+
+let isDragging = false; // 用於判斷是否正在拖動
+
+contactUs.addEventListener("mousedown", function (event) {
+  let shiftX = event.clientX - contactUs.getBoundingClientRect().left;
+  let shiftY = event.clientY - contactUs.getBoundingClientRect().top;
+
+  isDragging = false; // 初始化為 false
+
+  function moveAt(pageX, pageY) {
+    contactUs.style.left = pageX - shiftX + "px";
+    contactUs.style.top = pageY - shiftY + "px";
+  }
+
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    isDragging = true; // 拖動發生了
+    moveAt(event.pageX, event.pageY);
+  }
+
+  document.addEventListener("mousemove", onMouseMove);
+
+  document.addEventListener("mouseup", function onMouseUp() {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+
+    if (!isDragging) {
+      // 如果沒有拖動，則視為點擊
+      handleClick();
+    }
+  });
+
+  contactUs.ondragstart = function () {
+    return false;
+  };
+});
+
+// 點擊事件的處理函數
+function handleClick() {
+  alert("你點擊了聯繫我們的圖標！");
+  // 這裡可以放其他你想要執行的功能
+}
+//-----------------------Floating icon---------------------------------------
+
+const community = document.getElementById("community");
+let isDragging2 = false; // 用於判斷是否正在拖動
+
+community.addEventListener("mousedown", function (event) {
+  let shiftX = event.clientX - community.getBoundingClientRect().left;
+  let shiftY = event.clientY - community.getBoundingClientRect().top;
+
+  isDragging2 = false; // 初始化為 false
+
+  function moveAt(pageX, pageY) {
+    community.style.left = pageX - shiftX + "px";
+    community.style.top = pageY - shiftY + "px";
+  }
+
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    isDragging2 = true; // 拖動發生了
+    moveAt(event.pageX, event.pageY);
+  }
+
+  document.addEventListener("mousemove", onMouseMove);
+
+  document.addEventListener("mouseup", function onMouseUp() {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+
+    if (!isDragging2) {
+      // 如果沒有拖動，則視為點擊
+      handleClick();
+    }
+  });
+
+  community.ondragstart = function () {
+    return false;
+  };
+});
+
+// 點擊事件的處理函數
+function handleClick() {
+  alert("你點擊了聯繫我們的圖標！");
+  // 這裡可以放其他你想要執行的功能
+}
