d871813231a72b10e36c7b2e3cdc535be7f6400b
Michael <m24572582@gmail.com>
2024-12-03 23:15:28 +0800

    add game introduce page

---
+++ game_introduce/game_introduce.js
@@ -0,0 +1,15 @@
+function adjustOverlayHeight() {
+  const windowHeight = window.innerHeight - 0;
+  const newHeight = windowHeight - 200;
+  const blackOverlay = document.querySelector(".main_news_list");
+  if (blackOverlay) {
+    blackOverlay.style.height = `${newHeight}px`;
+  }
+}
+window.addEventListener("load", adjustOverlayHeight);
+window.addEventListener("resize", adjustOverlayHeight);
+
+function navigateToIndex(event) {
+  event.preventDefault(); // 阻止默認的超連結行為
+  location.replace("../index.html"); // 替換當前頁面的歷史記錄
+}
